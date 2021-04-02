d193730bb3c0c80bd6b077e65a1badc06967462a
chiefLeo <chiefleoxd@gmail.com>
2021-04-02 18:44:31 -0300

    A template already done and playable for coding a Minecraft mod.

---
+++ src/main/java/com/yourname/examplemod/Examplemod.java
@@ -0,0 +1,10 @@
+package com.yourname.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Examplemod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
